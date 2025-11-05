b8501e36770aa91b31f5875a4aff3b9ca476763b
GrowandBuild <Alexandrepessoalrodrigues@gmail.com>
2025-11-05 11:14:52 -0300

    s

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -53,7 +53,10 @@ class CategorySeeder extends Seeder
         ];
 
         foreach ($categories as $category) {
-            Category::create($category);
+            Category::firstOrCreate(
+                ['slug' => $category['slug']],
+                $category
+            );
         }
     }
 }
